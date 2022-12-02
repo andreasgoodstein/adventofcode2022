6734a0691d4bf5abfcbaac83db9711cd7e7af8ea
Andreas Goodstein <24245735+andreasgoodstein@users.noreply.github.com>
2022-12-02 16:55:56 +0100

    solve day 1

---
+++ day1/part2.ts
@@ -0,0 +1,22 @@
+import { load } from "../utils/file.ts";
+
+const input = await load("./day1/input.txt");
+
+let snackSum = 0;
+const topSnackAmount = input
+  .reduce<number[]>((acc, line) => {
+    if (!line) {
+      acc.push(snackSum);
+      snackSum = 0;
+      return acc;
+    }
+
+    snackSum += parseInt(line, 10);
+    return acc;
+  }, [])
+  .sort((a, b) => a - b)
+  .reverse()
+  .slice(0, 3)
+  .reduce((sum, snack) => sum + snack);
+
+console.log(topSnackAmount);
